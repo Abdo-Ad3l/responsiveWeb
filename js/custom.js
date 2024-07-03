fe053726dd20b96a78c1786d271d50cdc3e7df87
Abdo Adel <120386201+Abdo-Ad3l@users.noreply.github.com>
2024-07-03 03:20:19 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,31 @@
-/*global $ */
+// /*global $ */
 
 
-$(function () {
-    'use strict';
-    $('.info-list li').click(function () {
-        $(this).addClass('selected').siblings('li').removeClass('selected');
-        $('.info-content div').hide();
-        $('.' + $(this).data('class')).fadeIn();
-    });
-});
+// $(function () {
+//     'use strict';
+//     $('.info-list li').click(function () {
+//         $(this).addClass('selected').siblings('li').removeClass('selected');
+//         $('.info-content div').hide();
+//         $('.' + $(this).data('class')).fadeIn();
+//     });
+// });
+
+
+
+let liList = document.querySelectorAll(".info-list li");
+let divList = document.querySelectorAll(".info-content div");
+
+
+liList.forEach((li) => {
+    li.addEventListener("click", function ()  {
+        liList.forEach((li) => {
+            li.classList.remove("selected")
+            
+        })
+        this.classList.add("selected")
+        divList.forEach((div) => {
+            div.style.display = "none"
+        })
+        let divToShow = document.querySelector("." + this.dataset.class)
+        divToShow.style.display = "block"
+})})
